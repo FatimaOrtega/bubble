172adc8bde90c1748e9ea1b8c0637cb7b36b370f
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-08-09 20:04:52 -0500

    Created a navbar

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -3,7 +3,22 @@ import './nav.css'
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <nav className="nav">
+      <a href='/' className='site-title'>
+        BubbleGum Gemz
+      </a>
+      <ul>
+        <li>
+          <a href='/yasmin'>Yasmin</a> 
+        </li>
+        <li>
+        <a href='/about'>About Us</a> 
+        </li>
+        <li>
+        <a href='/contact'>Contact Information</a> 
+        </li>
+      </ul>
+      </nav>
   )
 }
 
---
+++ src/components/yasmin/Yasmin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './yasmin.css'
+
+const Yasmin = () => {
+  return (
+    <div>Yasmin</div>
+  )
+}
+
+export default Yasmin

79550c8ea92ddc85feb906f8659f1e1dd23a2de9
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-08-09 20:25:50 -0500

    Added navbar with working links

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Nav from './components/nav/nav'
 import Home from './components/home/Home'
+import About from './components/about/About'
 import Footer from './components/footer/Footer'
+import Yasmin from './components/yasmin/Yasmin'
+
+import Contact from './components/contact/Contact'
 
 const App = () => {
+  let component
+  switch (window.location.pathname) {
+    case '/':
+      component = <App />
+      break
+      case '/yasmin':
+        component = <Yasmin />
+      break
+      case '/about':
+        component = <About />
+      break
+      case '/contact':
+        component = <Contact />
+      break 
+  }
   return (
     <>
   
     <Nav />
-    <Home />
-    <Footer />
+    {component}
+    
    
     </>
 
